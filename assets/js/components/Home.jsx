4cc49ea1a1cbb7aa95d8ccc46e40f2c2f67d6f81
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-05-07 02:42:28 -0400

    Added home page buttons

--- assets/js/components/Home.jsx
+++ assets/js/components/Home.jsx
@@ -6,7 +6,20 @@ export default function Home(props) {
   return (
     <div id="home">
       <img src="/images/me.jpg" alt="Picture of Myself" />
-<i class="fab fa-jedi-order"></i>
+      <Row id="home-btns">
+        <Col md="4">
+          <i className="fas fa-book"></i>
+          <p>Coursework</p>
+        </Col>
+        <Col md="4">
+          <i className="fas fa-lightbulb"></i>
+          <p>Personal Projects</p>
+        </Col>
+        <Col md="4">
+          <i class="fas fa-envelope"></i>
+          <p>Contact</p>
+        </Col>
+      </Row>
     </div>
   );
 }
