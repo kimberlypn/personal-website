720abe491db39d370d7a1ad9c81ccf040f0b1cc9
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-05-08 22:24:33 -0400

    Added header and fixed centering

--- assets/js/components/Home.jsx
+++ assets/js/components/Home.jsx
@@ -4,22 +4,31 @@ import { Row, Col } from 'reactstrap';
 // Renders the home page
 export default function Home(props) {
   return (
-    <div id="home">
-      <img src="/images/me.jpg" alt="Picture of Myself" />
-      <Row id="home-btns">
-        <Col md="4">
-          <i className="fas fa-book"></i>
-          <p>Coursework</p>
-        </Col>
-        <Col md="4">
-          <i className="fas fa-lightbulb"></i>
-          <p>Personal Projects</p>
-        </Col>
-        <Col md="4">
-          <i class="fas fa-envelope"></i>
-          <p>Contact</p>
-        </Col>
-      </Row>
+    <div className="container parent" id="home">
+      <div className="child">
+        <h1>Kimberly Nguyen</h1>
+        <Row>
+          <img src="/images/me.jpg" alt="Picture of Myself" />
+        </Row>
+        <Row id="home-btns">
+          <Col md="3">
+            <i className="fas fa-user-circle"></i>
+            <p>About</p>
+          </Col>
+          <Col md="3">
+            <i className="fas fa-book"></i>
+            <p>Coursework</p>
+          </Col>
+          <Col md="3">
+            <i className="fas fa-lightbulb"></i>
+            <p>Personal Projects</p>
+          </Col>
+          <Col md="3">
+            <i className="fas fa-envelope"></i>
+            <p>Contact</p>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
