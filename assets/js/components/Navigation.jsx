a9b99810662af07025201867386ec50acd349f24
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-05-09 01:42:35 -0400

    Fixed styling

--- assets/js/components/Navigation.jsx
+++ assets/js/components/Navigation.jsx
@@ -20,7 +20,8 @@ export default function Navigation(props) {
         // Store hash
         var hash = this.hash;
         // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        // The optional number (800) specifies the number of milliseconds
+        // it takes to scroll to the specified area
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 500, function() {
