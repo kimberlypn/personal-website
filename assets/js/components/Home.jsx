9f217f7431ba3655f254a832262b72fc6256e8cf
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-05-08 22:39:32 -0400

    Added Paper component from Material UI

--- assets/js/components/Home.jsx
+++ assets/js/components/Home.jsx
@@ -1,34 +1,48 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
 
 // Renders the home page
 export default function Home(props) {
+  // Style for the Paper component
+  const style = {
+    height: 450,
+    width: 450,
+    display: 'block',
+    margin: 'auto'
+  };
+
   return (
-    <div className="container parent" id="home">
-      <div className="child">
-        <h1>Kimberly Nguyen</h1>
-        <Row>
-          <img src="/images/me.jpg" alt="Picture of Myself" />
-        </Row>
-        <Row id="home-btns">
-          <Col md="3">
-            <i className="fas fa-user-circle"></i>
-            <p>About</p>
-          </Col>
-          <Col md="3">
-            <i className="fas fa-book"></i>
-            <p>Coursework</p>
-          </Col>
-          <Col md="3">
-            <i className="fas fa-lightbulb"></i>
-            <p>Personal Projects</p>
-          </Col>
-          <Col md="3">
-            <i className="fas fa-envelope"></i>
-            <p>Contact</p>
-          </Col>
-        </Row>
+    <MuiThemeProvider>
+      <div className="container parent" id="home">
+        <div className="child">
+          <h1>Kimberly Nguyen</h1>
+          <Row>
+            <Paper style={style} zDepth={4} circle={true} >
+              <img src="/images/me.jpg" alt="Picture of Myself" />
+            </Paper>
+          </Row>
+          <Row id="home-btns">
+            <Col md="3">
+              <i className="fas fa-user-circle"></i>
+              <p>About</p>
+            </Col>
+            <Col md="3">
+              <i className="fas fa-book"></i>
+              <p>Coursework</p>
+            </Col>
+            <Col md="3">
+              <i className="fas fa-lightbulb"></i>
+              <p>Personal Projects</p>
+            </Col>
+            <Col md="3">
+              <i className="fas fa-envelope"></i>
+              <p>Contact</p>
+            </Col>
+          </Row>
+        </div>
       </div>
-    </div>
+    </MuiThemeProvider>
   );
 }
