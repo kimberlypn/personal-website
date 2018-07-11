8c3983554726fc0dc2e6ff94b3bfddccb023d2a6
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-07-10 21:45:37 -0400

    Added about me

--- assets/js/components/Home.jsx
+++ assets/js/components/Home.jsx
@@ -7,14 +7,26 @@ export default function Home(props) {
     <div id="home">
       <Row>
         <Col md="6">
+          <img src="/images/me.JPG" alt="Picture of Myself" />
+        </Col>
+        <Col md="6">
           <div id="tag-line">
             <p>I sleep, and I code things.</p>
             <span id="got-ref">(Yes, that was a Game of Thrones reference.)</span>
           </div>
+            <p id="blurb">
+              Hello, world! I am a software developer with an interest in
+              technology in health care. I graduated from Northeastern
+              University with a Bachelor of Science in computer science. Before
+              pursuing a degree in CS, I was majoring in health science. While
+              I am still driven to innovate this field, I wanted to make a
+              difference doing what I enjoy: building useful applications that
+              can make people's days that much better, whether that impact be
+              big or small. I love how prevalent CS is in almost anything and
+              the self-empowered problem-solving techniques that this field
+              demands.
+            </p>
 
-        </Col>
-        <Col md="6">
-          <img src="/images/me.JPG" alt="Picture of Myself" />
         </Col>
       </Row>
     </div>
