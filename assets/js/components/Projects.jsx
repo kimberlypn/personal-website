47602b6dc57390850d49e1aaacbe9388f22b8fdc
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-07-10 23:32:30 -0400

    Designed project cards

--- assets/js/components/Projects.jsx
+++ assets/js/components/Projects.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
+import { Card, CardImg } from 'reactstrap';
 
 /* Renders the projects page */
 export default function Projects(props) {
   return (
     <div id="projects">
-      projects
+      <Row>
+        <Col md="6" className="project-card">
+          <img src="/images/shambomon.png" alt="Shambomon" />
+          <div className="project-title">
+            <p>SHAMBOMON</p>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
