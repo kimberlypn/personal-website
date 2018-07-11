a8bfd8f1ea2a94cb30a5a1c95c36afc3fafd5bdd
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-07-11 10:31:00 -0400

    Added seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,54 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+defmodule Seeds do
+  alias PersonalWebsite.Repo
+  alias PersonalWebsite.Projects.Project
+
+  def insert_projects do
+    Repo.delete_all(Project)
+
+    Repo.insert!(%Project{
+      title: "Shambomon",
+      language: "Elixir",
+      type: "Class",
+      collaborators: "Kimberly Nguyen, Matt Dang",
+      start_date: ~D[2018-02-20],
+      end_date: ~D[2018-03-27],
+      img_src: "shambomon.png"
+    })
+    Repo.insert!(%Project{
+      title: "Goat Jumper",
+      language: "C++",
+      type: "Class",
+      collaborators: "Kimberly Nguyen, Max Mindel, Jason Fiammetta, Jerry Leung",
+      start_date: ~D[2018-01-30],
+      end_date: ~D[2018-04-17],
+      img_src: "goat-jumper.png"
+    })
+    Repo.insert!(%Project{
+      title: "TravelPal",
+      language: "Elixir",
+      type: "Class",
+      collaborators: "Kimberly Nguyen, Longsheng Lin, Matt Dang, William Guo",
+      start_date: ~D[2018-03-27],
+      end_date: ~D[2018-04-20],
+      img_src: "travelpal.png"
+    })
+  end
+
+  def run do
+    insert_projects()
+  end
+
+  def seed(:dev) do
+    run()
+  end
+
+  def seed(:prod) do
+    # run()
+  end
+end
+
+Seeds.seed(Mix.env)
