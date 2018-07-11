a669d5bdb21d9f2152b996369edbaba782fa0db0
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-07-10 21:36:51 -0400

    Added tagline Easter egg

--- assets/js/components/Home.jsx
+++ assets/js/components/Home.jsx
@@ -5,7 +5,18 @@ import { Row, Col } from 'reactstrap';
 export default function Home(props) {
   return (
     <div id="home">
-      <img src="/images/me.JPG" alt="Picture of Myself" />
+      <Row>
+        <Col md="6">
+          <div id="tag-line">
+            <p>I sleep, and I code things.</p>
+            <span id="got-ref">(Yes, that was a Game of Thrones reference.)</span>
+          </div>
+
+        </Col>
+        <Col md="6">
+          <img src="/images/me.JPG" alt="Picture of Myself" />
+        </Col>
+      </Row>
     </div>
   );
 }
