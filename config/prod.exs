474e244d5d98e6a2590d03ed270da75c51d41e2d
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-08-07 01:05:41 -0400

    Set up for Heroku deployment

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :personal_website, PersonalWebsiteWeb.Endpoint,
   url: [scheme: "https", host: "kimberlypn.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
--- lib/personal_website_web/endpoint.ex
+++ lib/personal_website_web/endpoint.ex
@@ -41,13 +41,12 @@ defmodule PersonalWebsiteWeb.Endpoint do
 
   @doc """
   Callback invoked for dynamically configuring the endpoint.
-
   It receives the endpoint configuration and checks if
   configuration should be loaded from the system environment.
   """
   def init(_key, config) do
     if config[:load_from_system_env] do
-      port = 5101 || raise "expected the PORT environment variable to be set"
+      port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
       {:ok, Keyword.put(config, :http, [:inet6, port: port])}
     else
       {:ok, config}
